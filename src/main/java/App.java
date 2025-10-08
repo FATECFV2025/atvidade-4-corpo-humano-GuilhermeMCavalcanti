dee3c8b44cc0d08d966bccf27b37fc7fb3d0a731
GuilhermeMCavalcanti <guilhermecavalcanto05@gmail.com>
2025-10-08 20:22:56 -0300

    Corpo humano e IMC
    Adicionado:
    Construtor, métodos getters e setters
    
    Entradas e saídas conforme o exemplo de execução do código

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,29 +2,49 @@ package main.java;
 import java.util.Scanner;
 
 public class App {
-        //Comentário
-
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in); 
-
-        //variável local a
-        String v2;
-        int v1;
-
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
-
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
     public static void main(String[] args) throws Exception {
-        funcao_teste();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== CRIAÇÃO DO OBJETO CORPO HUMANO ===");
+        System.out.print("Digite a massa inicial (kg): ");
+        float massa = sc.nextFloat();
+        System.out.print("Digite o volume inicial (m³): ");
+        float volume = sc.nextFloat();
+        System.out.print("Digite a densidade inicial (kg/m³): ");
+        float densidade = sc.nextFloat();
+        System.out.print("Digite a altura inicial (m): ");
+        float altura = sc.nextFloat();
+
+        CorpoHumano c1 = new CorpoHumano(massa, volume, densidade, altura);
+        c1.massa = "2"; 
+        //Questão 1
+        //Está dando erro pois massa é private e não pode ser acessada dessa forma
+
+        //Questão 2
+        //Agora massa pode ser acessada já que é public, mas o 2 nesse caso está como String, tendo uma disparidade com o tipo de dado (float)
 
 
+        System.out.println("=== VALORES INICIAIS ===");
+        System.out.printf("Massa: %.1f kg\n", c1.getMassa());
+        System.out.printf("Volume: %.2f m³\n", c1.getVolume());
+        System.out.printf("Densidade: %.1f kg/m³\n", c1.getDensidade());
+        System.out.printf("Altura: %.2f m\n", c1.getAltura());
+        System.out.printf("IMC: %.15f\n", c1.calcularIMC());
+
+        c1.setVolume(10);
+        //Questão 3
+        //Por estar privada, não é possível utilizar o método nessa classe
+
+        while (true) {
+        System.out.printf("Digite uma nova massa: ");
+        c1.setMassa(sc.nextFloat());
+        System.out.printf("Digite uma nova altura: ");
+        c1.setAltura(sc.nextDouble());
+
+        System.out.println("=== NOVOS VALORES ===");
+        System.out.printf("Nova massa: %.1f kg\n", c1.getMassa());
+        System.out.printf("Nova altura: %.2f m\n", c1.getAltura());
+        System.out.printf("Novo IMC: %.15f\n", c1.calcularIMC());
+        }
     }
 }
